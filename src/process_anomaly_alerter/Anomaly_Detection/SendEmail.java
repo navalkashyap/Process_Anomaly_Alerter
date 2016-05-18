82dbfc41e79d804f24bd1cb7e8745e038d007ec1
naval_ <naval_@DESKTOP-S228FC7>
2016-05-18 13:00:51 -0700

    Some changes

--- src/process_anomaly_alerter/Anomaly_Detection/SendEmail.java
+++ src/process_anomaly_alerter/Anomaly_Detection/SendEmail.java
@@ -5,7 +5,10 @@
  */
 package process_anomaly_alerter.Anomaly_Detection;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -15,12 +18,11 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendEmail {
-	public static void main(String[] args) {
+	void SendEmail(String msg){
 		Properties props = new Properties();
                 final String hostEmail="bellevue148@gmail.com";
-                final String hostPassword="";
-                String msg = "Hello World!";
-		props.put("mail.smtp.host", "smtp.gmail.com");
+                final String hostPassword="Bellevue@148";
+                props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
@@ -33,26 +35,22 @@ public class SendEmail {
 					return new PasswordAuthentication(hostEmail,hostPassword);
 				}
 			});
-
 		try {
-
 			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("bellevue148@gmail.com"));
+			message.setFrom(new InternetAddress("bellevue148@gmail.com", "Anomaly Detection Tool"));                        
 			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse("navalk@uw.edu"));
                         message.addRecipients(Message.RecipientType.CC,InternetAddress.parse("svasisht@uw.edu"));
                         message.addRecipients(Message.RecipientType.CC,InternetAddress.parse("swetha91@uw.edu"));
                         message.addRecipients(Message.RecipientType.CC,InternetAddress.parse("abirami@uw.edu"));
-                        
-                        message.setSubject("Testing Subject");
-			message.setText(msg +
-					"\n\n No spam to my email, please!");
-
+                        message.setSubject("Alert: Anomaly Processes Found in the Client Machine");
+			message.setText(msg + "\nThanks for using the tools\n");
 			Transport.send(message);
 
 			System.out.println("Done");
-
 		} catch (MessagingException e) {
 			throw new RuntimeException(e);
-		}
+		} catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(SendEmail.class.getName()).log(Level.SEVERE, null, ex);
+            }
 	}
 }
