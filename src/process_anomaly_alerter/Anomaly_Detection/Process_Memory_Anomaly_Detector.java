ed766d82744aea253b5d34645cda3d0dcd50a3ad
naval_ <naval_@DESKTOP-S228FC7>
2016-05-20 15:35:46 -0700

    Added package declaration on whitelist

--- src/process_anomaly_alerter/Anomaly_Detection/Process_Memory_Anomaly_Detector.java
+++ src/process_anomaly_alerter/Anomaly_Detection/Process_Memory_Anomaly_Detector.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.RandomAccessFile;
 import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -19,57 +20,81 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author naval_
  */
 public class Process_Memory_Anomaly_Detector {
     public static void main(String args[]){
-        String host="192.168.188.133";
+        while(true){
+            TrainDataSet("192.168.188.133");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Process_Memory_Anomaly_Detector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    static String computeRange(String input,String lowerrange,String higherrange){
+        String range=null;
+        float Range = Float.parseFloat(input);
+        float lowerRange = Float.parseFloat(lowerrange);
+        float higherRange = Float.parseFloat(higherrange);
+        if(Range<lowerRange)
+            lowerRange = Range;
+        else if(Range>higherRange)
+            higherRange = Range;
+        range = ","+Float.toString(lowerRange)+","+Float.toString(higherRange);
+        return range;
+    }
+    
+    static void TrainDataSet(String host){
         String trainedFile = host+"_trained.log";
         String SplitBy = ",";
         String newtrainedData = "";
         File readData = new File(host+".log");
-        File trainedData = new File(trainedFile);
+        String line,range;
         Path file = Paths.get(trainedFile);
         try {
-        Scanner train_Data = new Scanner(trainedData);
-        Scanner read_Data = new Scanner(readData);
-        int counter=0;
+            RandomAccessFile trainedData = new RandomAccessFile(new File(trainedFile),"rw");
+            Scanner read_Data = new Scanner(readData);           
+            long pointer=0;
             while(read_Data.hasNextLine()) {
-		String readLine = read_Data.nextLine();
+                String readLine = read_Data.nextLine();
                 String[] newData = readLine.split(SplitBy);
                 while(true){
-                    if(train_Data.hasNextLine()){
-                        String[] Data = train_Data.nextLine().split(SplitBy);
+                    pointer = trainedData.getFilePointer();
+                    if((line = trainedData.readLine()) != null){
+                        String[] Data = line.split(SplitBy);
                         if(Data[11].equals(newData[11])){
-                            System.out.println("yeah");
+                            range = computeRange(newData[4],Data[12],Data[13]);
+                            newtrainedData = newtrainedData+readLine+range+"\n";
+                            break;
                         }
                     } else {
-                            System.out.println(counter++);
-                        newtrainedData = newtrainedData+readLine+"\n";
+                        trainedData.seek(pointer);
+                        newtrainedData = newtrainedData+readLine+","+newData[4]+","+newData[4]+"\n";
                         break;
-                    }
-                    
+                    }                    
                 }
-                
             }
-        System.out.println(newtrainedData);
-        System.out.println();
-    Writer writer = null;
-    try {
-        writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(trainedFile), StandardCharsets.UTF_8));
-        writer.write(newtrainedData);
-    } catch (IOException ex) {
-        
-    } finally {
-        try {writer.close();} catch (Exception ex) {/*ignore*/}
-    }                       
+            trainedData.close();
         } catch (FileNotFoundException e) {
-		e.printStackTrace();
+            e.printStackTrace();
 	} catch (IOException e) {
-		e.printStackTrace();
-	} 
+            e.printStackTrace();
+	}
+        Writer writer = null;
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(trainedFile), StandardCharsets.UTF_8));       
+            writer.write(newtrainedData);
+        } catch (IOException ex) {
+        } finally {
+            try {writer.close();} catch (Exception ex) {/*ignore*/}
+        }
     }
 }
